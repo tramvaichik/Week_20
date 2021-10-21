3ecb6629e8d8694523028c3f6d5d4b2e4b998f40
tramvaichik <arsee@list.ru>
2021-10-21 15:27:11 +0300

    api  fixed syntax error

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,13 +1,13 @@
-// document.addEventListener("DOMContentLoader", function (event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
-fetch("https://api.nasa.gov/planetary/apod?api_key=JCUS23sYLZHk4p87qgVwiGyRN3ao3lPboQnHJuh0")
-    .then(response =>
-        response.json())
-    .then(planetary => {
-        document.getElementById("img").src = planetary.url;
-        document.getElementById("title").textContent = planetary.title;
-        document.getElementById("facts").textContent = planetary.explanation;
-    })
+    fetch("https://api.nasa.gov/planetary/apod?api_key=JCUS23sYLZHk4p87qgVwiGyRN3ao3lPboQnHJuh0")
+        .then(response =>
+            response.json())
+        .then(planetary => {
+            document.getElementById("img").src = planetary.url;
+            document.getElementById("title").textContent = planetary.title;
+            document.getElementById("facts").textContent = planetary.explanation;
+        })
 
-    .catch(error => console.log(error));
-// });
+        .catch(error => console.log(error));
+});
